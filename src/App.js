28616540f7965c7a214b22477bec047dfac49b6b
MoikeCheck <mikewheatley@hotmail.co.uk>
2022-03-17 16:29:21 +0000

    Firestore testing started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import { db } from "./firebase-config";
+import { collection, getDocs, addDoc } from "firebase/firestore";
 
 function App() {
+  const [newName, setNewName] = useState("");
+  const [newAge, setNewAge] = useState(0);
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "users");
+
+  const createUser = async () => {
+    await addDoc(usersCollectionRef, { name: newName, age: newAge });
+  };
+
+const updateUser = async (id, age) => {
+
+}
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      console.log(data);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getUsers();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        placeholder="Name..."
+        onChange={(e) => {
+          setNewName(e.target.value);
+        }}
+      />
+      <input
+        type="number"
+        placeholder="Age..."
+        onChange={(e) => {
+          setNewAge(e.target.value);
+        }}
+      />
+      <button onClick={createUser}>Create User</button>
+      {users.map((user) => {
+        return (
+          <div>
+            <h1>Name: {user.name} </h1>
+            <h1>Age: {user.age}</h1>
+            <button onClick={() => {}}>Increase Age</button>
+          </div>
+        );
+      })}
     </div>
   );
 }
---
+++ src/firebase-config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBS5WtxuaBO6G71D_Ehwtf-um5c2xCEK8U",
+  authDomain: "pet-app-9663f.firebaseapp.com",
+  databaseURL:
+    "https://pet-app-9663f-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "pet-app-9663f",
+  storageBucket: "pet-app-9663f.appspot.com",
+  messagingSenderId: "914531445977",
+  appId: "1:914531445977:web:7483d0374c17c1b65262c8",
+  measurementId: "G-RHS6PFDF92",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
